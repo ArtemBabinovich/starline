5f37e1128ccd5a582617fdb3257a40f32e377640
artem <zalivonka02@gmail.com>
2022-02-22 12:43:17 +0300

    upgrade models

--- starline/models.py
+++ starline/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class NumberPhone(models.Model):
+    """Номер и логотип для обратной связи"""
+    logo = models.ImageField('Логотип оператора связи')
+    numbers = models.CharField('Номер телефона', max_length=50)
+
+
+class OurWorks(models.Model):
+    """Фотографии и видео работ"""
+    picture = models.ImageField('Фотографии работ')
+    video_url = models.CharField('Видео работ')
+
+
+class Warranty_Support(models.Model):
+    """Гарантия и поддержка"""
+    description = models.TextField('Гарантия и поддержка')

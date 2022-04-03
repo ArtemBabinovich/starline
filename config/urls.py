ea285d98e64009a082d6cd32682949f2e3f7e376
artem <zalivonka02@gmail.com>
2022-04-03 17:25:46 +0300

    upgrade models.py and add serialeziers.py

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from starline import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 app_name = 'starline'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('starline.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    # path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 if settings.DEBUG:

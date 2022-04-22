fd94ad1f9a4b0203470c4f7bef9f3912ae9ca31d
artem <zalivonka02@gmail.com>
2022-04-22 15:05:39 +0300

    update js

--- starline/static/main_page/js/reviews.js
+++ starline/static/main_page/js/reviews.js
@@ -58,6 +58,7 @@ function getReviewText(){
                 const reviewCardLink = document.createElement('a');
                     reviewCardLink.className = 'review-card__link';
                     reviewCardLink.innerHTML = 'Подробнее';
+                    reviewCardLink.href = `https://starline.pythonanywhere.com/our_works/${item.slug}`;
                     //! тут надо добавить ссылку на карточку!!!
                     
                 item.append(reviewCardLink);

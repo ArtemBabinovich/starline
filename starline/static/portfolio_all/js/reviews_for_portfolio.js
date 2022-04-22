35085874b569575a27050aad08096a8b9ab05478
artem <zalivonka02@gmail.com>
2022-04-22 15:25:22 +0300

    fix bug

--- starline/static/portfolio_all/js/reviews_for_portfolio.js
+++ starline/static/portfolio_all/js/reviews_for_portfolio.js
@@ -63,7 +63,7 @@ function getReviewText(){
                 const reviewCardLink = document.createElement('a');
                     reviewCardLink.className = 'review-card__link';
                     reviewCardLink.innerHTML = 'Подробнее';
-                    reviewCardLink.href = `https://starline.pythonanywhere.com/our_work/${item.slug}`;
+                    reviewCardLink.href = `https://starline.pythonanywhere.com/ourwork/${item.slug}`;
                     //! тут надо добавить ссылку на карточку!!!
                     
                 item.append(reviewCardLink);
